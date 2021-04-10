06f8f354a4f7c78a8716c71b2b4bb38c4505d9e1
Rizaldr26 <80899593+Rizaldr26@users.noreply.github.com>
2021-04-10 23:56:37 +0900

    Add files via upload
    Rizal Dwiko Riyadi_202010370311134

---
+++ pbo2tgs/Main.java
@@ -0,0 +1,11 @@
+package com.pbo2tgs;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Benda produk = new Benda();
+        produk.menu();
+        produk.getnilaidata();
+    }
+
+}
